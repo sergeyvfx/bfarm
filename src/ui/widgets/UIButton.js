4f95ec0008e91cc65e346c9739caaa4938643502
Sergey I. Sharybin <g.ulairi@gmail.com>
2010-02-07 01:11:29 +0500

    Added sensivity to UIButton.

--- src/ui/widgets/UIButton.js
+++ src/ui/widgets/UIButton.js
@@ -42,6 +42,10 @@ function UIButton (opts)
 
       this.attachEvent (result, 'click', 'onClickHandler');
 
+      this.handleSensitive ();
+
+      this.handleSensitive (result);
+
       return result;
     };
 
@@ -50,7 +54,7 @@ function UIButton (opts)
    */
   this.onClickHandler = function (event)
     {
-      if (this.onClick)
+      if (this.sensitive && this.onClick)
         {
           this.onClick (event);
         }
@@ -100,14 +104,46 @@ function UIButton (opts)
       return this.title;
     };
 
-    /**
-     * Set button's title
-     */
-    this.setTitle = function (title)
+  /**
+   * Set button's title
+   */
+  this.setTitle = function (title)
     {
       this.title = title;
       this.titleElement.innerHTML = title;
     };
+
+  /**
+   * Setter of sensitive attribute
+   */
+  this.setSensitive = function (sensitive)
+    {
+      UIButton.prototype.setSensitive.call (this, sensitive);
+      this.handleSensitive ();
+    };
+
+  /**
+   * Handle sensitive attribute
+   */
+  this.handleSensitive  = function (dom)
+    {
+      dom = dom || this.dom;
+
+      if (!dom)
+        {
+          return;
+        }
+
+      if (this.sensitive)
+        {
+          $(dom).removeClass ('UIButtonInsesitive');
+        }
+      else
+        {
+          $(dom).addClass ('UIButtonInsesitive');
+        }
+    };
+
   /* Title which will be displayed */
   this.title = opts['title'];
 
